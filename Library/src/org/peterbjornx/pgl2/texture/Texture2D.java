706d1f2b96a6b62834bba75722924d2b27aa184b
Peter Bosch <peterbosc@gmail.com>
2011-06-16 18:46:29 +0200

    - Implemented basic texture2D wrapper

--- Library/src/org/peterbjornx/pgl2/texture/Texture2D.java
+++ Library/src/org/peterbjornx/pgl2/texture/Texture2D.java
@@ -1,7 +1,11 @@
 package org.peterbjornx.pgl2.texture;
 
 import org.peterbjornx.pgl2.internal.GLAttribute;
+import org.peterbjornx.pgl2.util.ServerMemoryManager;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import java.nio.ByteBuffer;
 import java.util.LinkedList;
 
 /**
@@ -12,10 +16,54 @@ import java.util.LinkedList;
  * Provides an easy interface to OpenGL 2D Textures
  * @author Peter Bosch (AKA Peterbjorn)
  */
+
+
+
 public class Texture2D {
 
     private int texturePtr;
+    private boolean imageLoaded = false;
+    private int width;
+    private int height;
+    private int format;
+    private int bufFormat;
+    private ByteBuffer pixels;
     private LinkedList<GLAttribute> attributes = new LinkedList<GLAttribute>();
+    private int serverMemoryUsage = 0;
+
+    public Texture2D(int width, int height, int format, int bufFormat, ByteBuffer pixels) {
+        this.width = width;
+        this.height = height;
+        this.format = format;
+        this.bufFormat = bufFormat;
+        this.pixels = pixels;
+        texturePtr = glGenTextures();
+    }
+
+    public void enable(){
+        glEnable(GL_TEXTURE_2D);
+    }
+
+    public void disable(){
+        glDisable(GL_TEXTURE_2D);
+    }
+
+    public void bind(){
+        glBindTexture(GL_TEXTURE_2D,texturePtr);
+        if (!imageLoaded){
+            glTexImage2D(GL_TEXTURE_2D,0,format,width,height,0,bufFormat,GL_UNSIGNED_BYTE,pixels);
+            serverMemoryUsage+=pixels.limit();
+            pixels=null;
+            ServerMemoryManager.textureMemory+=serverMemoryUsage;
+            imageLoaded = true;
+        }
+    }
 
 
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        pixels = null;
+        ServerMemoryManager.requestTextureDeletion(texturePtr, serverMemoryUsage);
+    }
 }
